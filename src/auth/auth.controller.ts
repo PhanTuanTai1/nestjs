e41174890cf02cd558ec4400fecafcfbec93aadf
phantuantai1234@gmail.com <phantuantai1234@gmail.com>
2021-05-01 22:18:24 +0700

    video 59

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('/signup')
+    async signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+        return await this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('/signin') 
+    async signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+        return await this.authService.signIn(authCredentialsDto);
+    }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,43 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import {EntityRepository, Repository} from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import {User} from "./user.entity";
+import * as bcrypt from "bcrypt";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    async signUp(authCredentialsDto : AuthCredentialsDto) : Promise<void> {
+        const {username, password} = authCredentialsDto;
+
+        try {
+            let user = new User();
+            user.username = username;
+            user.salt = await bcrypt.genSalt();
+            user.password = await this.hashPassword(password, user.salt);
+            await user.save();
+        }
+        catch(error) {
+            if(error.code === '23505' ) {
+                throw new ConflictException('username already exists');
+            }
+            else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    async hashPassword(password: string, salt: string) {
+        return await bcrypt.hash(password, salt)
+    }
+
+    async validateUserPassword(authCredentialsDto : AuthCredentialsDto) : Promise<string> {
+        const {username, password} = authCredentialsDto;
+        let user = this.findOne({username});
+
+        if(user && await (await user).validatePassword(password)) {
+            return (await user).username;
+        }
+        
+        return null;
+    }
+}
